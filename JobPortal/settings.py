6c9e65fb1f0505dafbf6cd79b49b660a638d01b5
Victor Amonde <victor.amonde@gmail.com>
2024-10-14 10:12:47 +0300

    project development progress

--- JobPortal/settings.py
+++ JobPortal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5e-^t26_qy8z@26bbfi#n_gjnr8a#9t@g@hvfqsbn+dm6(%=ml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
